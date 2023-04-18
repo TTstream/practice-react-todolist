314174a51565c71d75083fd28411046cf9982b5a
SeongJin Oh <guk0230@naver.com>
2023-04-18 11:28:09 +0900

    todo 클릭시 toggle조정

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,15 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-function Todo({ task }) {
+function Todo({ task, toggleComplete }) {
   return (
     <div className="Todo">
-      <p>{task.task}</p>
+      <p
+        onClick={() => toggleComplete(task.id)}
+        className={`${task.completed ? "completed" : ""}`}
+      >
+        {task.task}
+      </p>
       <div>
         <FontAwesomeIcon icon={faPenToSquare} />
         <FontAwesomeIcon icon={faTrash} />
